c116f56df7bb9957315264cb30413d72e46a90ff
abdus251 <abdus251@gmail.com>
2024-08-19 19:49:16 +0600

    star-rating has been complited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      {/* this is accordian component */}
-     {/* <Accordian /> */}
+      {/* accordian component */}
+      {/* <Accordian /> */}
+      {/* {Random color component} */}
 
-     {/* {Random color component} */}
-     <RandomColor />
+
+      {/* <RandomColor /> */}
+<StarRating />
+
+
     </div>
   );
 }
