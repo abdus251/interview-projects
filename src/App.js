1a7be620269759976a53c46f84ad4a552ec2cf6b
abdus251 <abdus251@gmail.com>
2024-08-20 14:22:12 +0600

    load more products poject has complited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import LoadMoreData from './components/5-load-more-data';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -9,11 +11,20 @@ function App() {
       {/* accordian component */}
       {/* <Accordian /> */}
       {/* {Random color component} */}
-
-
       {/* <RandomColor /> */}
-<StarRating />
 
+      {/* Star rating component */}
+      {/* <StarRating /> */}
+
+      {/* Image slider component */}
+      {/* <ImageSlider url={'https://restcountries.com/v3.1/all'} 
+      limit={'10'}/> */}
+
+
+
+
+{/* Load more products components */}
+<LoadMoreData />
 
     </div>
   );
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from 'react';
 import './style.css';
+export default function ImageSlider({ url, limit }) {
 
+    const [images, setImages] = useState([])
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [loading, setLoading] = useState(false)
+    async function fetchImages(getUrl) {
+        try {
+            setLoading(true);
 
-export default function ImageSlider({url})
+            const response = await fetch(getUrl);
+            const data = await response.json();
+            
+            if (data) {
+                setImages(data);
+                setLoading(false);
+            }
+        } catch (e) {
+            setErrorMsg(e.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if (url !== '') fetchImages(url)
+    }, [url]);
+
+    if (loading) {
+        return <div>Loading data ! Please wait</div>
+    }
+
+    if (errorMsg !== null) {
+        return <div>Error occured ! {errorMsg} </div>;
+    }
+
+    return <div className="container"></div>;
+}
